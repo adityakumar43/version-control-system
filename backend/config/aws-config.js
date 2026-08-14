62cc605926f65136b3fc7a609bb0d360dcda9b36
Aditya <kumaradityap123@gmail.com>
2026-08-15 01:34:34 +0530

    completd aws config

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const AWS = require("aws-sdk");
 
-AWS.config.update({ region: "ap-south-1" });
+AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 const s3 = new AWS.S3();
 const S3_BUCKET = "apnagithubclone";
