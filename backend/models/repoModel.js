00a62dd94169d61f7f4047fb1d02c2303b281cdb
Aditya <kumaradityap123@gmail.com>
2026-08-23 16:16:33 +0530

    completed repo model

---
+++ backend/models/repoModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const RepositorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+    },
+    content: [
+        {
+            type: String,
+        },
+    ],
+    visibility: {
+        type: Boolean,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    issues: [
+        {
+            type: String,
+            ref: "Issue"
+        },
+    ],
+});
+
+const Repository = mongoose.model("Repository", RepositorySchema);
+export default Repository;
