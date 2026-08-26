3891ad3aaf622376ecd5ea4de47d3d463eeb5828
Aditya <kumaradityap123@gmail.com>
2026-08-27 00:21:16 +0530

    issues router created

---
+++ backend/routes/issue.router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const issueController = require("../controllers/issueController");
+
+const issueRouter = express.Router();
+
+issueRouter.post("/issue/create", issueController.createIssue);
+issueRouter.get("/issue/all", issueController.getAllIssuees);
+issueRouter.get("/issue/:id", issueController.getIssueById);
+issueRouter.put("/issue/update/:id", issueController.updateIssueById);
+issueRouter.delete("/issue/delete/:id", issueController.deleteIssueById);
+
+
+module.exports = issueRouter;
