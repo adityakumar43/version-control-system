3c5401977d3ff8a223912a2c8e9d3be3a6fe4f2a
Aditya <kumaradityap123@gmail.com>
2026-08-29 17:53:17 +0530

    modified in userProfile route

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -4,10 +4,10 @@ const userController = require("../controllers/userController");
 const userRouter = express.Router();
 
 userRouter.get("/allUsers", userController.getAllUsers);
-userRouter.get("/userProfile", userController.getUserProfile);
+userRouter.get("/userProfile/:id", userController.getUserProfile);
 userRouter.post("/signup", userController.signup);
 userRouter.post("/login", userController.login);
-userRouter.put("/updateProfile", userController.updateUserProfile);
-userRouter.delete("/deleteProfile", userController.deleteUserProfile);
+userRouter.put("/updateProfile/:id", userController.updateUserProfile);
+userRouter.delete("/deleteProfile/:id", userController.deleteUserProfile);
 
 module.exports = userRouter;
