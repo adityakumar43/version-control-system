e10bb24e950433fabec71f243b1fd4b81b6b0222
Aditya <kumaradityap123@gmail.com>
2026-09-02 18:06:48 +0530

    completed create issue function

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -1,20 +1,42 @@
-const createIssue = ( req, res) => {
-    res.send("Issue created!");
+const mongoose = require("mongoose");
+const Repository = require("../models/repoModel");
+const User = require("../models/userModel");
+const Issue = require("../models/issueModel");
+
+
+async function createIssue( req, res){
+    const {id} = req.params;
+    const {title, description} = req.body;
+    try {
+        const issue = new Issue({
+        title,
+        description,
+        repository: id,
+    });
+
+    await issue.save();
+
+    res.status(201).json(issue);
+
+    } catch (error) {
+        console.error("Error during issue creation:", error.message);
+        res.status(500).send("Server error");
+    }
 };
 
-const updateIssueById = ( req, res) => {
+async function updateIssueById( req, res){
     res.send("Issue updated!");
 };
 
-const deleteIssueById = ( req, res) => {
+async function deleteIssueById( req, res){
     res.send("Issue delete!");
 };
 
-const getAllIssuees = ( req, res) => {
+async function getAllIssuees( req, res){
     res.send("All Issues Fetched!");
 };
 
-const getIssueById = ( req, res) => {
+async function getIssueById( req, res){
     res.send("Issue Details fetched!");
 };
 
